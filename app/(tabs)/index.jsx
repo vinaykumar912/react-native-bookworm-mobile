b4bd4ad892e8b79853dc6f0918613a65816fb382
vinaykumar912 <mailtovinay912@gmail.com>
2025-08-23 00:29:25 +0530

    changes

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -108,7 +108,7 @@ export default function Home() {
     return stars;
   };
 
-  if (isLoading) return <Loader />;
+  if (isLoading && !data) return <Loader />;
 
   if (isError)
     return (
@@ -132,7 +132,7 @@ export default function Home() {
         showsVerticalScrollIndicator={false}
         refreshControl={
           <RefreshControl
-            refreshing={isRefetching}
+            refreshing={isRefetching && !isLoading}
             onRefresh={refetch} // refetch + shuffle again
             colors={[COLORS.primary]}
             tintColor={COLORS.primary}
